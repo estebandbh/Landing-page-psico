726012af6a52389a709b32f9cc09faab19291b3e
estebandbh <120118077+estebandbh@users.noreply.github.com>
2023-02-03 18:00:44 -0500

    Delete codigo.js

--- codigo.js
+++
@@ -1,7 +0,0 @@
-let iconMenu = document.querySelector(".icon__bar");
-let listaMenu = document.querySelector(".lista__menu");
-
-iconMenu.addEventListener("click", () => {
-  listaMenu.classList.toggle("disable");
-});
- 
